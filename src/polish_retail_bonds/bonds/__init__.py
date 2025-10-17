77e72633339b584a3dafd26d03d29c5168fa8f27
Jakub Kuczys <me@jacken.men>
2025-10-17 22:14:14 +0200

    Add `__all__` to `bonds` submodule

--- src/polish_retail_bonds/bonds/__init__.py
+++ src/polish_retail_bonds/bonds/__init__.py
@@ -10,3 +10,17 @@ from .known_products import (
     create_tos_bond,
 )
 from .monetary_values import MonetaryValues
+
+__all__ = (
+    "Bond",
+    "InterestPeriod",
+    "InterestRate",
+    "create_coi_bond",
+    "create_dor_bond",
+    "create_edo_bond",
+    "create_rod_bond",
+    "create_ror_bond",
+    "create_ros_bond",
+    "create_tos_bond",
+    "MonetaryValues",
+)
